7eb5fcc9ac93d89279ddc8eb315dd06393dd08fc
Sammy Shaar <Sammy Shaar>
2017-10-05 21:51:37 -0400

    wip got options passed

--- test/axe.js
+++ test/axe.js
@@ -8,17 +8,15 @@ describe('axe test', function () {
 
     // inject the script
     browser.execute(axeSource);
-
+    // browser.debug();
+    var options = {runOnly: {type: "tag", values: ["wcag2a"]}};
     // run inside browser and get results
-    let results = browser.executeAsync(function (done) {
-      // run axe on our site
-      axe.run(function (err, results) {
-        if (err) done(err)
-        done(results);
-      });
-    });
-
+    let results = browser.executeAsync(function (options, done) {axe.run(function (err, results) { done(results);});}, {runOnly: {type: "tag", values: ["wcag2a"]}});
+  
     // assert there are no violations
+    console.log(results.value, 'values')
+    console.log(results.value.violations, 'vol')
+    console.log(results.value.violations.length, 'len')
     assert.equal(results.value.violations.length, 0, 'Expected no a11y violations');
   })
 })
